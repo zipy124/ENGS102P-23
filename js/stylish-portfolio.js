7f330518f404951402f8c3214c12161cbb953220
zipy124 <iiianmmm@gmail.com>
2017-11-09 17:13:21 +0000

    changed scroll amount, changed body padding
    This accounts for the removal of the search bar in the navigation strip

--- js/stylish-portfolio.js
+++ js/stylish-portfolio.js
@@ -20,7 +20,7 @@
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top-70
+          scrollTop: target.offset().top-55
         }, 1000, "easeInOutExpo");
         return false;
       }
